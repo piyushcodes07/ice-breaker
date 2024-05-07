e674901d8218641f92ca1aaa378249b72217e985
piyushcodes07 <piyushcodes07@gmail.com>
2024-05-08 02:48:13 +0530

    linkedIn scraper finished and integrated to chain

--- ice_breaker.py
+++ ice_breaker.py
@@ -3,15 +3,14 @@ from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
+from linkedIn.linkedIn import scrape_linkedIn
 
 if __name__ == "__main__":
     load_dotenv()
     print(os.environ['OPENAI_API_KEY'])
-    information = """
-    bacchuwar is a cse student at ghrce college
-    """
+
     summary_template = """
-    given the information {information} about a person i want you to create 
+    given the LinkedIn information {information} about a person i want you to create 
     1. a short summary
     2. two interesting facts about them
     """
@@ -19,7 +18,10 @@ if __name__ == "__main__":
     summary_prompt_template = PromptTemplate(
         input_variables=["information"],template=summary_template
     )
+
+    linkedIn_data= scrape_linkedIn(mock=True)
+
     llm = ChatOpenAI(temperature=0,model_name="gpt-3.5-turbo")
     chain=LLMChain(llm=llm,prompt=summary_prompt_template)
-    res=chain.invoke(input={"information":information})
+    res=chain.invoke(input={"information":linkedIn_data})
     print(res)
---
+++ linkedIn/linkedIn.py
@@ -0,0 +1,29 @@
+import os
+import requests
+
+def scrape_linkedIn(linkedIn_profile_url: str="https://gist.githubusercontent.com/piyushcodes07/00ffbeddfded48f4bca19006cd5a52d3/raw/788bea03da4a0193c04bc6c191913cf62a4df274/gistfile1.txt", mock:bool=False):
+
+    if mock:
+        res=requests.get(linkedIn_profile_url,timeout=10)
+
+        data=res.json()
+
+        # data={
+        #     k:v
+        #     for k,v in data.items()
+        #     if v not in ([],"",None," ")
+        #     and k not in ["certifications"]
+        # }
+        cleaned_data={}
+        for k,v in data.items():
+            if k not in ["certifications"] and v not in [[],""," ",None]:
+                cleaned_data[k]=v
+
+        return cleaned_data
+
+    else:
+        print("mock is set to false.")
+
+if __name__=="__main__":
+    data=scrape_linkedIn(mock=True)
+    print(data)
